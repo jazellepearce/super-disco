729c1aab0a8e992d8a7d8a595cb09b5df4a48629
Jazelle Pearce <pjazelle@yahoo.com>
2022-04-24 17:29:39 -0500

    local storage added

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,29 @@
+var task = document.getElementsByClassName('.description');
 var currentDate = new Date();
 $("#currentDay").html(currentDate);
 
-$(document).ready(function () {
-    // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+/*var saveTasks = function() {
+    localStorage.setItem('.description', JSON.stringify(this.items));
+}
+*/
 
-        // Save text in local storage
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr(".hour");
+
         localStorage.setItem(time, text);
     })
+
+    $("#am9 .description").val(localStorage.getItem("am9"));
+    $("#am10 .description").val(localStorage.getItem("am10"));
+    $("#am11 .description").val(localStorage.getItem("am11"));
+    $("#pm12 .description").val(localStorage.getItem("pm12"));
+    $("#pm1 .description").val(localStorage.getItem("pm1"));
+    $("#pm2 .description").val(localStorage.getItem("pm2"));
+    $("#pm3 .description").val(localStorage.getItem("pm3"));
+    $("#pm4 .description").val(localStorage.getItem("pm4"));
+    $("#pm5 .description").val(localStorage.getItem("pm5"));
+
 })
+
