325cbae6f827d687176f3a893f5f46eddd35afbd
Jazelle Pearce <pjazelle@yahoo.com>
2022-04-24 15:57:22 -0500

    html added

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,14 @@
 var currentDate = new Date();
 $("#currentDay").html(currentDate);
 
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+})
